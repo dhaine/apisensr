5060f74b1fde8c4f3395ee5bdea5eadbc7739954
Denis Haine <denis.haine@gmail.com>
2020-02-27 21:21:45 -0500

    Perf (package) Stop app when session end.

--- R/app_server.R
+++ R/app_server.R
@@ -2,12 +2,11 @@
 source("inst/app/www/pop_up.R")
 app_server <- function(input, output,session) {
     # List the first level callModules here
-    # get started button clicked
-#    observeEvent(input$show_help,{
-#        pop_up('Welcome to apisensr', 'inst/app/www/get_started.html', 'Get started')
-#        })
-    ## observe show intro event which is triggered on start from custom.js
     observe({
                 pop_up('apisensr & episensr', 'inst/app/www/start.html', "Let's go!")
-    })
+            })
+
+    ## Automatically stop Shiny app when closing browser tab
+    session$onSessionEnded(stopApp)
+
 }
