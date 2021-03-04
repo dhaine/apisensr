92f376e8bb549beee4ef76306424671b85af2eb2
Denis Haine <denis.haine@gmail.com>
2021-03-03 22:30:23 -0500

    Fix (namespace) ggplot2 imports.

--- R/mod_prob.R
+++ R/mod_prob.R
@@ -15,6 +15,7 @@
 #' @keywords internal
 #' @export
 #' @import episensr
+#' @import ggplot2
 #' @importFrom shiny NS tagList
 #' @importFrom shinyjs runjs
 #' @importFrom rhandsontable hot_to_r rHandsontableOutput renderRHandsontable rhandsontable
