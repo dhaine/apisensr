b6d52e3c52b8ba2f19820cee57c0b6b85ad9df3d
Denis Haine <denis.haine@gmail.com>
2021-03-09 20:31:48 -0500

    Fix/Add (apisensr doc) Use properly roxygen to create apisensr pkg doc.

---
+++ R/apisensr-package.R
@@ -0,0 +1,21 @@
+#' Interface to 'apisensr' for sensitivity analysis of epidemiological results
+#'
+#' @description API for using 'episensr' which provides basic sensitivity analysis
+#' of the observed relative risks adjusting for unmeasured confounding and
+#' misclassification of the exposure/outcome, or both.
+#'
+#' @seealso Useful links:
+#' \itemize{
+#' \item \url{https://github.com/dhaine/apisensr}
+#' \item Report bugs at \url{https://github.com/dhaine/apisensr/issues}
+#' \item episensr on R CRAN \url{https://cran.r-project.org/package=episensr}
+#' }
+#'
+#' @docType package
+#' @name apisensr
+#'
+#' @examples
+#' \dontrun{
+#' run_app()
+#' }
+NULL
